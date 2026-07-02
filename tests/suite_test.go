807feac0b862a4ad3eb658fa1ab6a6b00f10a9ef
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-07-01 21:52:13 -0500

    Testing environment (#2)
    * Initial test impl
    
    * Initial vibe
    
    * Attr moved
    
    * PR comments
    
    * Change my mind
    
    * ope

---
+++ tests/suite_test.go
@@ -0,0 +1,35 @@
+package integration_test
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+)
+
+var (
+	ctx     context.Context
+	cfg     *rest.Config
+	testEnv *envtest.Environment
+)
+
+func TestIntegration(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Integration Suite")
+}
+
+var _ = BeforeSuite(func() {
+	ctx = context.Background()
+	testEnv = &envtest.Environment{}
+	var err error
+	cfg, err = testEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(cfg).NotTo(BeNil())
+})
+
+var _ = AfterSuite(func() {
+	Expect(testEnv.Stop()).To(Succeed())
+})
